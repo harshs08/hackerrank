8f12018f5c11d8963fb39f01dbb7af4eb97c8950
Harsh Singh <harshsin@usc.edu>
2015-01-29 16:51:06 -0800

    rewrote the algo with sliding window concept

--- algorithmns/AngryChildren.java
+++ algorithmns/AngryChildren.java
@@ -2,33 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 // The part of the program involving reading from STDIN and writing to STDOUT has been provided by us.
 
 public class AngryChildren {
 	static BufferedReader in = new BufferedReader(new InputStreamReader(
 			System.in));
-	//static StringBuilder out = new StringBuilder();
+	static StringBuilder out = new StringBuilder();
 
-	public static int smallestDif(int[] range, int count){
-		int minValue = 0;
-		int length = range.length;
-		int tempDiff = Integer.MAX_VALUE;
-		int min = Integer.MAX_VALUE;
-		int minInd = 0;
-
-		for (int i=0; i<length-1 ; i++) {
-			for (int j=i+1; j<length; j++) {
-				tempDiff = Math.abs(range[j] - range[i]);			
-				if(tempDiff<min){
-					min = tempDiff;
-					if(range[i] <= range[j]) minInd = i;
-					else minInd = j;
-				} 	
+	//Using the concept of sliding window
+	public static int smallestDiff(int[] packets, int numKids){
+		int minValue = Integer.MAX_VALUE;
+		Arrays.sort(packets);
+		for(int i = 0; i < packets.length - numKids + 1; i ++)
+		{
+			int temp = packets[i + numKids - 1] - packets[i];
+			if(temp < minValue)
+			{
+				minValue = temp;
 			}
 		}
-		System.out.println("Min diff= "+min);
-		System.out.println("Min Value= "+range[minInd]);
 		return minValue;
 	}
 
@@ -47,7 +42,7 @@ public class AngryChildren {
 		
 		  // Write your code here, to process numPackets N, numKids K, and the packets of candies
 		// Compute the ideal value for unfairness over here
-		smallestDif(packets, numKids);
+		unfairness = smallestDiff(packets, numKids);
 		System.out.println(unfairness);
 	}
 }
