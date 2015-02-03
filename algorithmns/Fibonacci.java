32d6b5883ae15df9c9b54ed06e84c0c63432afe4
Harsh Singh <harshsin@usc.edu>
2015-02-02 17:40:56 -0800

    second attempt using square check

--- algorithmns/Fibonacci.java
+++ algorithmns/Fibonacci.java
@@ -6,14 +6,26 @@ import java.util.regex.*;
 
 public class Fibonacci {
 
-		public static String checkFibonacci(Integer num){
+		public static String isFibonacci(Long num){
+			Long a = (Long)Math.sqrt(5*num*num+4);
+			Long b = (Long)Math.sqrt(5*num*num-4);
+
+			if(Math.pow(a,2)==(5*num*num +4) || Math.pow(b,2)==(5*num*num -4)){
+				return "IsFibo";
+			}
+			else{
+				return "IsNotFibo";
+			}
+		}
+
+		public static String checkFibonacci(Long num){
 			if(num == 1 || num == 2){
 				return "IsFibo";
 			}
-			int a,b,c;
-			a=0;
-			b=1;
-			c=0;
+			Long a,b,c;
+			a=0L;
+			b=1L;
+			c=0L;
 			while(c<num){
 				c = a+b;
 				a = b;
@@ -33,12 +45,24 @@ public class Fibonacci {
 			int t = Integer.parseInt(in.nextLine());
 			int i;
 
-			Integer[] input = new Integer[t];
+			Long[] input = new Long[t];
 
 			for (i=0; i<t; i++) 
-				input[i] = Integer.parseInt(in.nextLine());
+				input[i] = Long.parseLong(in.nextLine());
 
+			// for (i=0; i<t; i++) 
+			// 	System.out.println(checkFibonacci(input[i]));
+			// for (i=0; i<t; i++) {
+			// 	if(isFibonacci(input[i])){
+			// 		System.out.println("IsFibo");
+			// 	}
+			// 	else{
+			// 		System.out.println("IsNotFibo");
+			// 	}
+			// }
 			for (i=0; i<t; i++) 
-				System.out.println(checkFibonacci(input[i]));
+				System.out.println(isFibonacci(input[i]));
+
+			in.close();
 		}
 }
