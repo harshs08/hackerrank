aa791ad7cd5bd30a73fabdab19ace28b43662f2e
Harsh Singh <harshsin@usc.edu>
2015-02-09 01:12:22 -0800

    completed the pangram problem

--- algorithmns/Strings/Pangrams.java
+++ algorithmns/Strings/Pangrams.java
@@ -4,9 +4,21 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Pangrams {
+
+	public static String checkPanagram(String sent){
+		sent = sent.toLowerCase();
+		for (char a = 'a'; a < 'z'; a++) {
+			if(sent.indexOf(a) < 0)
+				return "not pangram";
+		}
+		return "pangram";
+	}
 
 	public static void main(String[] args) {
 		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		Scanner in = new Scanner(System.in);
+		String sentance = in.nextLine();
+		System.out.println(checkPanagram(sentance));
 	}
 }
